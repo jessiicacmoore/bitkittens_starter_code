e06cc74eb868e1504583a31bdd20b559962b54d0
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-20 22:12:33 -0400

    Complete

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const catsBtn = document.querySelector('.summon-cats');
+  var catBoxes = document.querySelectorAll('.cat-box');
+
+  catsBtn.addEventListener('click', () => {
+    axios.get('http://bitkittens.herokuapp.com/cats.json')
+      .then(resp => {
+        let cats = resp.data.cats;
+
+        cats.forEach((cat, index) => {
+          catImg = document.createElement('img');
+          catImg.src = cat.photo;
+          catImg.alt = `Photo of ${cat.name}`;
+          catBoxes[index].innerHTML = "";
+          catBoxes[index].appendChild(catImg);
+        })
+      })
+  })
 });
